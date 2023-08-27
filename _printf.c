35ff8463b2d99b6cb483e01859991ee19ab132e3
KiellyE <ezekielimereti@gmail.com>
2023-08-27 13:46:55 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,101 +1,45 @@
-include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
-
 #include "main.h"
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
- */
-int _printf(const char *format, ...)
-int _printf(const char * const format, ...)
-{
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+* _printf - this function calls other functions.
+* @format: identifier to look for.
+* Return: the length of the output.
+*/
 
-	if (format == NULL)
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+int _printf(const char *format, ...)
+{
+	func_handler fh[] = {
+	{"%c", char_speci}, {"%s", str_speci}, {"%%", mdlus_speci},
+	{"%i", int_speci}, {"%d", deci_speci}, {"%b", bin_speci},
+	{"%u", unsigned_speci}, {"%o", octal_speci}, {"%x", hex_speci},
+	{"%X", HEX_speci}, {"%S", xstring_speci}, {"%p", pointer_speci},
+	{"%r", strrev_speci}, {"%R", rot13ed_speci}
 	};
 
-	va_list args;
-	int i = 0, j, len = 0;
+	int i = 0, h, length_printed = 0;
+	va_list listed_args;
 
-	va_start(args, format);
+	va_start(listed_args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-Here:
+Main:
 	while (format[i] != '\0')
 	{
-		if (format[i] != '%')
-		j = 13;
-		while (j >= 0)
+		h = 13;
+		while (h >= 0)
 		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			if (fh[h].id[1] == format[i + 1] && fh[h].id[0] == format[i])
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				length_printed += fh[h].f(listed_args);
+				i+=2;
+				goto Main;
 			}
-			j--;
-			}
-		_putchar(format[i]);
-		len++;
+			h--;
+		}
+		_std_output(format[i]);
+		length_printed++;
 		i++;
 	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
-}
-
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-	va_end(args);
-	return (len);
+	va_end(listed_args);
+	return (length_printed);
 }
